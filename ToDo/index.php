b3a724a822f127c9cabe286a87c7f1abfcc064cd
VadymShcherbak <vadymshcherback92@gmail.com>
2021-05-14 17:03:10 +0300

    todo start

---
+++ ToDo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+	<title>Task</title>
+</head>
+<body>
+	<div class="main-section">
+		<div class="container">
+			<div class="wrapper row">
+				<div class="col-lg-6 offset-lg-3">
+					<form action="" method="post">
+						<input type="text" class="form-control" name="" id="">
+						<button type="submit" class="btn btn-primary">Add task</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
